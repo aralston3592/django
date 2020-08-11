223ac16401cd4cc6b26a687159e78c8a79723e6d
Andrew Ralston <andrewralston35@gmail.com>
2020-08-11 21:29:27 +0000

    form testing completed

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Item
-from .forms import Itemform
+from todo.forms import Itemform
 # Create your views here.
 
 
@@ -37,3 +37,16 @@ def edit_item(request, item_id):
         'form': form
     }
     return render(request, 'todo/edit_item.html', context)
+
+
+def toggle_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.done = not item.done
+    item.save()
+    return redirect('get_todo_list')
+
+
+def delete_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.delete()
+    return redirect('get_todo_list')
